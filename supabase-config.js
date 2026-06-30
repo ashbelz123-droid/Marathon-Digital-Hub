25ac8f234bc030c380c07262a85ef6d44fff33d3
ashbelz123-droid <ashbelz123@gmail.com>
2026-06-30 15:39:34 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://sfimuvisljmezpajxxpf.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_Lg_uS6NX4c8kd8Vr6FnIZw_GyNS4SwY";
+
+const supabase =
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
