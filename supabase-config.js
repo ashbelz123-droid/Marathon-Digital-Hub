2ce2b1b3e5330ad97569320a49036278f37b4808
ashbelz123-droid <ashbelz123@gmail.com>
2026-06-30 16:10:58 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,9 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 "sb_publishable_Lg_uS6NX4c8kd8Vr6FnIZw_GyNS4SwY";
 
-const supabase = window.supabase.createClient(
+const { createClient } = supabase;
+
+window.supabaseClient = createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
