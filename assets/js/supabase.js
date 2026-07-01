955bc77d882590686368445c42ed16bdc3bf3b80
ashbelz123-droid <ashbelz123@gmail.com>
2026-07-01 08:55:37 +0300

    Delete assets/js/supabase.js

--- assets/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://wcrrxsrtbelcbycppieg.supabase.co";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcnJ4c3J0YmVsY2J5Y3BwaWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MTM0ODQsImV4cCI6MjA5NjE4OTQ4NH0.yh_xCiEW2T9riiDJgEWBmIU4V0e5azRQ0sITaEhfCo4";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
